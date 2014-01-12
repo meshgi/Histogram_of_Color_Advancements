1e86622971781fdff34c18afc48ce5c92b8885df
meshgi <kouroshmeshgi@gmail.com>
2014-01-13 01:57:12 +0900

    Template Update Era
    * Average of "Last 5" HOCs using a queue
    * Average of all HOCs so far
    * "Template Update with Memory"

--- template_update.m
+++ template_update.m
@@ -6,6 +6,10 @@ function new_template = template_update ( method , old_template , new_entry , fr
         return;
     end
     
+    
+    persistent q_counter;
+    persistent q_hoc;
+    
     switch (method)
         case 'none'
             new_template = old_template;
@@ -14,8 +18,35 @@ function new_template = template_update ( method , old_template , new_entry , fr
         case 'moving average'
             new_template = 0.9*old_template + 0.1*new_entry;
         case 'last 5'
-            % enqueue the new_entry
-            % calculate average of all queue elements
+            q_size = 5;
+            if (frame_no == 2)
+                q_counter = 2;
+                q_hoc = [old_template; new_entry];
+            else
+                if (q_counter < q_size)
+                    q_hoc = [q_hoc ; new_entry];
+                    q_counter = q_counter + 1;
+                else
+                    q_hoc = [q_hoc(2:end,:) ; new_entry];
+                end
+            end
+            new_template = mean(q_hoc);
+                
+        case 'update with memory'
+            alpha = 0.1; %short term memory forgetting rate
+            beta = 0.4; %long term memory forgetting rate
+            long_interval = 10;
+                        
+            if ( isempty(q_hoc) )
+                q_hoc = old_template;
+            end
+            
+            if ( mod(frame_no,long_interval) == 0 )
+                q_hoc = (1-beta)*q_hoc + beta*new_entry;
+            end
+            
+            new_template = 0.5*(q_hoc + (1-alpha)*old_template + alpha*new_entry);
+            
         case 'average all'
             % TO BE CHECKED
             n = frame_no;
